c1a53e9a1c8f82ba4d56f63741c27fa400154a09
SL1900 <43700351+SL1900@users.noreply.github.com>
2024-05-22 19:55:15 +0900

    Initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        config = function () 
+            require("telescope").setup({
+                pickers = { find_files = { hidden = true } },
+                extensions = {
+                    file_browser = {
+                        hijack_netrw = true,
+                        grouped = true,
+                    },
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown(),
+                    },
+                },
+            })
+            pcall(require("telescope").load_extension, "fzf")
+            pcall(require("telescope").load_extension, "ui-select")
+        end
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+}
