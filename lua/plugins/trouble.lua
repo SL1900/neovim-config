32f9f8381279bf1689ddc492f53b78d013cbaf38
David Work <david_super@inbox.ru>
2024-07-09 17:23:25 +0900

    trouble hotkeys enabled

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,17 @@
 return {
     {
         "folke/trouble.nvim",
-        config = true,
+        config = function()
+            vim.keymap.set("n", "<leader>xx", function()
+                trouble.toggle("workspace_diagnostics") 
+            end, { desc = "Toggle Trouble" })
+            vim.keymap.set("n", "<leader>xq", function()
+                trouble.toggle("quickfix")
+            end, { desc = "Toggle [Q]uickfix list" })
+            vim.keymap.set("n", "<leader>xl", function()
+                trouble.toggle("loclist")
+            end, { desc = "Toggle [L]oc list" })
+        end,
         opts = {
         }
     },
