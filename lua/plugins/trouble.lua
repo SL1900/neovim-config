55007b0264ebf5886caef614d585fb6002587a58
David Work <david_super@inbox.ru>
2024-07-09 17:49:18 +0900

    trouble hotkeys enabled (correctly this time)

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,18 +1,39 @@
 return {
     {
         "folke/trouble.nvim",
-        config = function()
-            vim.keymap.set("n", "<leader>xx", function()
-                trouble.toggle("workspace_diagnostics") 
-            end, { desc = "Toggle Trouble" })
-            vim.keymap.set("n", "<leader>xq", function()
-                trouble.toggle("quickfix")
-            end, { desc = "Toggle [Q]uickfix list" })
-            vim.keymap.set("n", "<leader>xl", function()
-                trouble.toggle("loclist")
-            end, { desc = "Toggle [L]oc list" })
-        end,
-        opts = {
-        }
+        config = true,
+        keys = {
+            {
+              "<leader>xx",
+              "<cmd>Trouble diagnostics toggle<cr>",
+              desc = "Diagnostics (Trouble)",
+            },
+            {
+              "<leader>xX",
+              "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+              desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+              "<leader>cs",
+              "<cmd>Trouble symbols toggle focus=false<cr>",
+              desc = "Symbols (Trouble)",
+            },
+            {
+              "<leader>cl",
+              "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+              desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+              "<leader>xL",
+              "<cmd>Trouble loclist toggle<cr>",
+              desc = "Location List (Trouble)",
+            },
+            {
+              "<leader>xQ",
+              "<cmd>Trouble qflist toggle<cr>",
+              desc = "Quickfix List (Trouble)",
+            },
+        },
+        opts = {}
     },
 }
