6129d0cf1eaea8872132266ca031eadba3636bb3
David Work <david_super@inbox.ru>
2024-07-10 09:19:42 +0900

    Ensure main treesitter languages are installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,8 @@ return {
             vim.cmd("TSUpdate")
             require("nvim-treesitter.install").compilers = { "zig", "gcc", "cc", "cl", "clang" }
             require("nvim-treesitter.configs").setup({
-                highlight = { enable = true }
+                highlight = { enable = true },
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typescript", "javascript", "css", "scss", "html", "svelte", "json", "yaml" }
             })
         end
     }
