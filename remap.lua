1d3b684636e90e9158153caee98f3b8916e5f2b4
SL1900 <david_super@inbox.ru>
2024-07-21 19:16:06 +0900

    No telescope remap if telescope is not initialized

--- remap.lua
+++ remap.lua
@@ -5,36 +5,38 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
-vim.keymap.set("n", "<leader>bc", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=. select_buffer=true<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>bb", builtin.buffers, { noremap = true })
-vim.keymap.set("n", "<leader>/", function()
-	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-		winblend = 10,
-		previewer = false
-	}))
-end,
-{ desc = "[/] Fuzzily search in curent buffer" })
-vim.keymap.set("n", "<leader>s/", function()
-	builtin.live_grep({
-		grep_open_files = true,
-		prompt_title = "Live Grep in Open Files"
-	})
-end,
-{ desc = "[S]earch [/] in Open files" })
-vim.keymap.set("n", "<leader>sn", function()
-	builtin.find_files({ cwd = vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/" })
-end,
-{ desc = "[S]earch [N]eovim files" })
+if builtin then
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
+    vim.keymap.set("n", "<leader>bc", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {noremap=true})
+    vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=. select_buffer=true<CR>", {noremap=true})
+    vim.keymap.set("n", "<leader>bb", builtin.buffers, { noremap = true })
+    vim.keymap.set("n", "<leader>/", function()
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            winblend = 10,
+            previewer = false
+        }))
+    end,
+    { desc = "[/] Fuzzily search in curent buffer" })
+    vim.keymap.set("n", "<leader>s/", function()
+        builtin.live_grep({
+            grep_open_files = true,
+            prompt_title = "Live Grep in Open Files"
+        })
+    end,
+    { desc = "[S]earch [/] in Open files" })
+    vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files({ cwd = vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/" })
+    end,
+    { desc = "[S]earch [N]eovim files" })
+end
 
 --LSP
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
