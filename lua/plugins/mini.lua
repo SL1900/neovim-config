7705d15ad11652728142fbf26680d24628d0c4e8
SL1900 <43700351+SL1900@users.noreply.github.com>
2024-09-14 02:50:13 +0900

    Enable "mini" modules: notify, sessions, starter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,8 +4,11 @@ return {
         require("mini.ai").setup({ n_lines = 500 })
         local statusline = require("mini.statusline")
         statusline.setup({ use_icons = vim.g.have_nerd_font })
-        statusline.section_localtion = function()
-            return "%21:%-2v"
-        end
+        -- statusline.section_location = function()
+        --     return "%21:%-2v"
+        -- end
+        require("mini.notify").setup()
+        require("mini.sessions").setup()
+        require("mini.starter").setup()
     end
 }
