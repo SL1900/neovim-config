5b390f052ac915990182e447d0f27e7b017b83c0
SL1900 <43700351+SL1900@users.noreply.github.com>
2024-11-28 00:30:32 +0900

    Make all floating windows fully transparent (kudos to 0xfraso on github)

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -2,9 +2,26 @@ return {
     "xiyaowong/transparent.nvim",
     config = function ()
         require("transparent").setup({
-            extra_groups = {
-                "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-                "NvimTreeNormal" -- NvimTree
+            extra_groups = { -- table/string: additional groups that should be cleared
+                "BufferLineTabClose",
+                "BufferlineBufferSelected",
+                "BufferLineFill",
+                "BufferLineBackground",
+                "BufferLineSeparator",
+                "BufferLineIndicatorSelected",
+
+                "IndentBlanklineChar",
+
+                -- make floating windows transparent
+                "LspFloatWinNormal",
+                "Normal",
+                "NormalFloat",
+                "FloatBorder",
+                "TelescopeNormal",
+                "TelescopeBorder",
+                "TelescopePromptBorder",
+                "SagaBorder",
+                "SagaNormal",
             },
         })
     end
