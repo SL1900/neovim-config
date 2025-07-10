3c1cef8993b1f6a3e86ec511a2dbedab41ce0b44
SL1900 <david_super@inbox.ru>
2025-07-10 13:46:00 +0900

    Remove automatic installation or rocks

--- lua/plugins/luarocks.lua
+++ lua/plugins/luarocks.lua
@@ -1,6 +1,7 @@
 return {
     "vhyrro/luarocks.nvim",
+    priority = 1000,
     opts = {
-        rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+        -- rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
     }
 }
