10da971a63364f8829467e716fec7c2aad62913a
SL1900 <43700351+SL1900@users.noreply.github.com>
2025-07-21 21:27:57 +0900

    Added telescope-undo plugin, changed telescope plugin file to include it's extension separatly

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        config = function ()
+        config = function()
             require("telescope").setup({
                 pickers = {
                     find_files = {
@@ -33,30 +33,60 @@ return {
                             width = 0.75,
                         },
                     },
-               },
+                },
                 extensions = {
                     file_browser = {
                         hijack_netrw = true,
                         grouped = true,
                     },
-                    fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                        case_mode = "smart_case",
-                    },
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown(),
-                    },
                 },
-           })
-            pcall(require("telescope").load_extension, "fzf")
-            pcall(require("telescope").load_extension, "ui-select")
+            })
         end
     },
     {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+        build =
+        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        opts = {
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+            }
+        },
+        config = function(_, opts)
+            require("telescope").setup(opts)
+            pcall(require("telescope").load_extension, "fzf")
+        end
     },
-    { "nvim-telescope/telescope-ui-select.nvim" },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        opts = {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown(),
+                }
+            }
+        },
+        config = function(_, opts)
+            require("telescope").setup(opts)
+            require("telescope").load_extension("ui-select")
+        end
+    },
+    {
+        "debugloop/telescope-undo.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            extensions = {
+                undo = {}
+            }
+        },
+        config = function(_, opts)
+            require("telescope").setup(opts)
+            require("telescope").load_extension("undo")
+        end
+    }
 }
