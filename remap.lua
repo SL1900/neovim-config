e6cc2fe9ecbbefe106a09b6641c840093723d7a5
SL1900 <david_super@inbox.ru>
2025-07-29 00:50:37 +0900

    Added keymap q to close current QuickFix buffer

--- remap.lua
+++ remap.lua
@@ -89,5 +89,13 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 -- vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end, { desc = "Toggle [L]oc list" })
 --
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", {desc="[T]ab [O]pen"})
-vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", {desc="[T]ab [C]lose"})
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "[T]ab [O]pen" })
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "[T]ab [C]lose" })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "qf" },
+    callback = function()
+        vim.schedule(function()
+            vim.keymap.set("n", "q", ":q<cr>", { desc = "Close current QuickFix buffer" })
+        end)
+    end
+})
