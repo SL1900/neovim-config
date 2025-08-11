d9805a5eded90bf852d8508faaf8fb645d7879be
SL1900 <david_super@inbox.ru>
2025-08-12 01:59:29 +0900

    Added snacks plugin by folke

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        image = {
+            enabled = true,
+        },
+        indent = {
+            enabled = true,
+        },
+        input = {
+            enabled = true,
+        },
+        picker = {
+            enabled = true,
+        },
+        quickfile = {
+            enabled = true,
+        },
+        rename = {
+            enabled = true,
+        },
+        scope = {
+            enabled = true,
+        },
+    },
+}
