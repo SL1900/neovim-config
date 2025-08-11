5437ea938ffbe02298cebeb867b309d5521f911e
SL1900 <david_super@inbox.ru>
2025-08-11 17:49:03 +0900

    Added telescope-live-grep-args plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -95,5 +95,27 @@ return {
             require("telescope").setup(opts)
             require("telescope").load_extension("undo")
         end
-    }
+    },
+    {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        config = function()
+            local lga_actions = require("telescope-live-grep-args.actions")
+            require("telescope").setup({
+                extensions = {
+                    live_grep_args = {
+                        auto_quoting = true,
+                        mappings = {
+                            i = {
+                                ["<C-k>"] = lga_actions.quote_prompt(),
+                                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob "}),
+                                ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                            }
+                        }
+                    }
+                }
+            })
+            require("telescope").load_extension("live_grep_args")
+            vim.keymap.set("n", "<leader>fg", ":lua require(\"telescope\").extensions.live_grep_args.live_grep_args()<CR>")
+        end
+    },
 }
