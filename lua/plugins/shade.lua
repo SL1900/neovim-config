6993fb729f2b6e1ce3cbeb8d3107b10f1c9d23b7
SL1900 <43700351+SL1900@users.noreply.github.com>
2025-08-12 01:54:46 +0900

    Replaces "shade" plugin with "tint" plugin

--- lua/plugins/shade.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- "valentino-sm/shade.nvim",
-    -- config = function ()
-    --     require("shade").setup({
-    --         overlay_opacity = 50,
-    --         opacity_step = 1,
-    --     })
-    -- end
-}
