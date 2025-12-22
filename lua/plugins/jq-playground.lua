e0cb572962313e67199308e92ff0e0b98491f077
SL1900 <david_super@inbox.ru>
2025-12-22 16:10:35 +0900

    Added jq-playground plugin setup (opts commented out because broke somehow)

--- lua/plugins/jq-playground.lua
+++ lua/plugins/jq-playground.lua
@@ -1,3 +1,9 @@
 return {
     "yochem/jq-playground.nvim",
+    opts = {
+        -- cmd = { "jq", "-n" },
+    },
+    config = function (_, opts)
+        require("jq-playground").setup(opts)
+    end
 }
