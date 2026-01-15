e1588c24964bde81ac5c725ccc0d7d925b9e580a
SL1900 <43700351+SL1900@users.noreply.github.com>
2026-01-15 22:35:36 +0900

    Suppress "Content Modified" errors from powershell lsp

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -2,7 +2,18 @@ return {
     {
         "ray-x/lsp_signature.nvim",
         event = "VeryLazy",
-        opts = {},
+        opts = {
+            ignore_error = function (err, ctx, config)
+                if ctx and ctx.client_id then
+                    local client = vim.lsp.get_client_by_id(ctx.client_id)
+                    if client and vim.tbl_contains({ "powershell_es" }, client.name) then
+                        if err.message == "Content Modified" then
+                            return true
+                        end
+                    end
+                end
+            end
+        },
         config = function (_, opts) require("lsp_signature").setup(opts) end
     },
 }
