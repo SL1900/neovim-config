06093193b8885873597cbd005e0b6d753bfb4272
SL1900 <43700351+SL1900@users.noreply.github.com>
2026-08-02 22:37:56 +0900

    Plugin config change: diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,10 @@
 return {
     "dlyongemallo/diffview-plus.nvim",
+    -- dependencies = {
+    --     "sindrets/diffview.nvim",
+    -- },
+    version = "*",
+    -- config = function ()
+    --     require("diffview-plus").setup()
+    -- end
 }
