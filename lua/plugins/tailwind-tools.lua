3eb8f1484c128cc0005b1491b088d756bb6cdd58
SL1900 <43700351+SL1900@users.noreply.github.com>
2026-08-02 22:35:59 +0900

    Using different tailwind-tools repo

--- lua/plugins/tailwind-tools.lua
+++ lua/plugins/tailwind-tools.lua
@@ -1,6 +1,7 @@
 return {
     -- "luckasRanarison/tailwind-tools.nvim",
     "lg-epitech/tailwind-tools.nvim",
+    "MurkyTheMurloc/tailwind-tools.nvim",
     name = "tailwind-tools",
     build = ":UpdateRemotePlugins",
     opts = {},
